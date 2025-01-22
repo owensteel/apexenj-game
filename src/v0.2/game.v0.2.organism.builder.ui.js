37f9ca4604d071a5fd20a0a47b1836d2439bc5fc
Owen Alistair Steel <woofdog04@gmail.com>
2025-01-22 16:15:18 +0000

    Adjusts hex grid visual

--- src/v0.2/game.v0.2.organism.builder.ui.js
+++ src/v0.2/game.v0.2.organism.builder.ui.js
@@ -16,7 +16,7 @@ import { getGlobalBoundingBoxOfHTMLElement } from "./game.v0.2.utils";
 // Hexagons
 
 const hexGrid = {
-    side: 11,
+    side: 11.25,
     hexPositions: [],
     hexTable: {},
     image: null
