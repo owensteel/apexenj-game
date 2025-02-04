86733133ffaa592ec24d786b570c5e30c31be567
Owen Alistair Steel <woofdog04@gmail.com>
2025-02-04 01:42:00 +0000

    New Main object model

--- src/v1/game.v1.main.js
+++ src/v1/game.v1.main.js
@@ -8,65 +8,78 @@ import { BLOCK_TYPENAME_HEART, BLOCK_TYPENAME_MOTOR } from "./game.v1.blocks"
 import DNA from "./game.v1.dna"
 import Organism from "./game.v1.organism"
 import Pool from "./game.v1.pool"
-import { DNA_NODE_ROLE_APPENDAGE, DNA_NODE_ROLE_ROOT, UPDATES_PER_SEC } from "./game.v1.references"
+import { DNA_NODE_ROLE_APPENDAGE, DNA_NODE_ROLE_ROOT } from "./game.v1.references"
 
-function init() {
+class Main {
+    constructor(presetPoolData = {}) {
+        this.presetPoolData = presetPoolData
 
-    // Create Pool
-
-    const currentPool = new Pool("123")
-
-    // Demo DNA
-
-    const demoDna = new DNA(
-        DNA_NODE_ROLE_ROOT,
-        BLOCK_TYPENAME_HEART,
-        [
-            new DNA(),
-            new DNA(),
-            new DNA(),
-            new DNA(
-                DNA_NODE_ROLE_APPENDAGE,
-                BLOCK_TYPENAME_MOTOR
-            ),
-            new DNA(),
-            new DNA(
-                DNA_NODE_ROLE_APPENDAGE,
-                BLOCK_TYPENAME_MOTOR
+        // Create or open Pool
+        if (this.presetPoolData) {
+            this.currentPool = new Pool(
+                this.presetPoolData.id,
+                this.presetPoolData.organisms
             )
-        ]
-    )
+        } else {
+            this.currentPool = new Pool()
+        }
+    }
+    init() {
+        const currentPool = this.currentPool
 
-    // Create demo organisms
+        // Demo DNA
 
-    const organism1 = new Organism(
-        // Clone demo
-        new DNA(
+        const demoDna = new DNA(
+            DNA_NODE_ROLE_ROOT,
+            BLOCK_TYPENAME_HEART,
+            [
+                new DNA(),
+                new DNA(),
+                new DNA(),
+                new DNA(
+                    DNA_NODE_ROLE_APPENDAGE,
+                    BLOCK_TYPENAME_MOTOR
+                ),
+                new DNA(),
+                new DNA(
+                    DNA_NODE_ROLE_APPENDAGE,
+                    BLOCK_TYPENAME_MOTOR
+                )
+            ]
+        )
+
+        // Create demo organisms
+
+        const organism1 = new Organism(
+            // Clone demo
+            new DNA(
+                demoDna.role,
+                demoDna.block.typeName,
+                demoDna.children
+            )
+        )
+        currentPool.addOrganism(organism1)
+
+        const organism2Dna = new DNA(
             demoDna.role,
             demoDna.block.typeName,
             demoDna.children
         )
-    )
-    currentPool.addOrganism(organism1)
+        const organism2 = new Organism(organism2Dna)
+        currentPool.addOrganism(organism2)
 
-    const organism2Dna = new DNA(
-        demoDna.role,
-        demoDna.block.typeName,
-        demoDna.children
-    )
-    const organism2 = new Organism(organism2Dna)
-    currentPool.addOrganism(organism2)
+        // Update loop
 
-    // Update loop
+        const UPDATES_PER_SEC = 24
+        setInterval(() => {
+            currentPool.updateLife()
+        }, 1000 / UPDATES_PER_SEC)
 
-    const UPDATES_PER_SEC = 24
-    setInterval(() => {
-        currentPool.updateLife()
-    }, 1000 / UPDATES_PER_SEC)
+        // Debug log
 
-    // Debug log
-
-    console.log(currentPool)
+        console.log(currentPool)
+        window.cL = currentPool
+    }
 }
 
-export { init }
+export default Main
