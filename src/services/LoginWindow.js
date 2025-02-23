43b593adad6747a6c9e03c74533f3be85e958443
Owen Alistair Steel <woofdog04@gmail.com>
2025-02-23 00:13:10 +0000

    Sends login request message instead of opening iframe

--- src/services/LoginWindow.js
+++ src/services/LoginWindow.js
@@ -8,14 +8,8 @@ const websiteBaseURL = window.location.hostname == "localhost" ? "http://localho
 
 class LoginWindow {
     constructor(whenCompletedAction = () => { }) {
-        // Open frame
-        const loginWindowWrapper = document.createElement("login-window-wrapper")
-        const loginWindowFrame = document.createElement("iframe")
-        loginWindowFrame.className = "login-window-frame"
-        loginWindowFrame.src = `${websiteBaseURL}/account/signin/?origin=play.apexenj.com&ref=game_client`
-        loginWindowWrapper.appendChild(loginWindowFrame)
-        document.body.appendChild(loginWindowWrapper)
-
+        // Open window
+        window.parent.postMessage("loginRequested", "*");
         // Handle login completion event
         window.addEventListener('message', (event) => {
             if (event.origin == websiteBaseURL && event.data == "loginCompleted") {
